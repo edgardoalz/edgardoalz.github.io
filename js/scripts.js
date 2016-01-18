eb953c1d00e3275d6a11190013ee8dd55ab183d8
Edgardo Alvarez <edalvz@gmail.com>
2016-01-17 17:01:46 -0700

    Deteccion de la pagina actual en el menu

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function paginaActiva() {
+
+    var dir = window.location.href;
+    var host = window.location.protocol + "//" + window.location.host;
+    var ruta = dir.substring(host.length);
+    var archivos, activo = false;
+
+    var menu = document.getElementById('menu');
+    var elementos = menu.childNodes;
+
+    for (i = 0; i < elementos.length; i++) {
+        var nodos = elementos[i].childNodes;
+
+        for (j = 0; j < nodos.length; j++){
+
+            if (nodos[j].tagName === "A") {
+                var url = nodos[j].getAttribute("href");
+
+                if (url === ruta) {
+                    activo = true;
+                    elementos[i].className = "activo";
+                    break;
+                
+                } else if (url === "/archivos/") {
+                    archivos = elementos[i];
+                }
+            }
+        }
+    }
+    if (!activo) {
+        archivos.className = "activo";
+    }
+}
+// Ejecutando la funcion
+paginaActiva();
