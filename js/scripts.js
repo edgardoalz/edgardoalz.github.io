8984e1298efac9f50b8267092a269d2dd13263c1
Edgardo Alvarez <edgardoalz@protonmail.com>
2025-08-12 23:14:29 -0700

    feat: implement pagefind

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 (function () {
 
-    // set search element
-    var searchBtn = document.getElementById('searchBtn');
-    var searchTxt = document.getElementById('searchTxt');
-    var data = document.getElementById('datos');
     function activePage () {
         var dir = window.location.href;
         var host = window.location.protocol + "//" + window.location.host;
@@ -40,47 +36,12 @@
     // Ejecutando la funcion
     activePage();
 
-    function getArticles () {
-        return new Promise (function (resolve, reject) {
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == XMLHttpRequest.DONE) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else if (xhr.status == 400) {
-                        reject('Error 404');
-                    } else {
-                        reject('Error: ' + xhr.status);
-                    }
-                }
-            };
-
-            xhr.open("GET", "/index.json", true);
-            xhr.send();
-        }) ;
-
-    }
-
-
-    searchBtn.addEventListener('click', search);
-    searchTxt.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            search();
-        }
-    });
-    function search () {
-        getArticles().then(function (articles) {
-            var regex = new RegExp(searchTxt.value, 'ig');
-            var filter = articles.filter(function (article) {
-                return regex.test(article.summary) || regex.test(article.title);
-            });
-            console.log(data);
-            console.log(filter);
-        }).catch(function (error) {
-            console.warn(error);
+    window.addEventListener('load', function() {
+        new PagefindUI({ 
+            element: "#search",
+            showImages: false
         });
-    }
+    });
 
     window.onload = function (ev) {
         var content = document.querySelector('#contenido');
